f0b55fb8171acc972a270340fdd77ac983cbeebf
Dan <dtmoodie>
2015-09-28 11:23:09 -0400

    Added auto resizing of BufferPool if a currently used buffer is requested. Added some boost logging.

--- EagleLib/src/CudaUtils.hpp
+++ EagleLib/src/CudaUtils.hpp
@@ -1,38 +1,27 @@
+/*
+Copyright(c) 2015, Daniel Moodie
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met :
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and / or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #pragma once
 
 #include <opencv2/core/cuda.hpp>
 #include <boost/thread/recursive_mutex.hpp>
 #include <queue>
 #include <boost/thread.hpp>
+#include <boost/log/trivial.hpp>
 namespace EagleLib
 {
-
-template<typename T>
-struct GpuResized
-{
-	GpuResized()
-	{
-		cudaMalloc(&GpuSetSize, sizeof(int));
-		cudaMemset(GpuSetSize, 0x00, sizeof(int));
-	}
-	/*GpuResized(GpuResized<T>& other)
-	{
-		GpuSetSize = other.GpuSetSize;
-		data = other.data;
-	}
-	GpuResized& operator=(GpuResized<T>& other)
-	{
-		GpuSetSize = other.GpuSetSize;
-		data = other.data;
-	}*/
-	~GpuResized()
-	{
-
-	}
-	T data;
-	// This is the size that is set from the gpu
-	int* GpuSetSize;
-};
 
 template<typename T> void cleanup(T& ptr, typename std::enable_if< std::is_array<T>::value>::type* = 0) { /*delete[] ptr;*/ }
 template<typename T> void cleanup(T& ptr, typename std::enable_if< std::is_pointer<T>::value && !std::is_array<T>::value>::type* = 0) { delete ptr; }
@@ -290,17 +279,25 @@ template<typename Data>
             boost::recursive_mutex::scoped_lock lock(mtx);
             if(size == 0)
                 return nullptr;
-            return &buffer[putItr++%size];
+			auto itr = putItr++%size;
+			if (!buffer[itr]->ready())
+			{
+				BOOST_LOG_TRIVIAL(warning) << "Buffer not ready, increasing size of buffer pool";
+				resize(buffer.size() + 1);
+			}
+            return &buffer[itr];
         }
         Buffer<T>* getBack()
-        {
-            boost::recursive_mutex::scoped_lock lock(mtx);
-            if(!buffer[getItr%size].ready())
-                return nullptr;
-			if (getItr == putItr)
-				return nullptr;
-            return &buffer[getItr++%size];
-        }
+		{
+			boost::recursive_mutex::scoped_lock lock(mtx);
+			auto itr = getItr++%size;
+			if (!buffer[itr].ready())
+			{
+				BOOST_LOG_TRIVIAL(warning) << "Buffer not ready, increasing size of buffer pool";
+				resize(buffer.size() + 1);
+			}
+			return &buffer[itr];
+		}
         Buffer<T>* waitBack()
         {
             boost::recursive_mutex::scoped_lock lock(mtx);
@@ -348,14 +345,24 @@ template<typename Data>
 			boost::recursive_mutex::scoped_lock lock(mtx);
 			if (size == 0)
 				return nullptr;
-			return &buffer[putItr++%size];
+			auto itr = putItr++%size;
+			if (!buffer[itr]->ready())
+			{
+				BOOST_LOG_TRIVIAL(warning) << "Buffer not ready, increasing size of buffer pool";
+				resize(buffer.size() + 1);
+			}
+			return &buffer[itr];
 		}
 		Buffer<T, P1>* getBack()
 		{
 			boost::recursive_mutex::scoped_lock lock(mtx);
-			if (!buffer[getItr%size].ready())
-				return nullptr;
-			return &buffer[getItr++%size];
+			auto itr = getItr++%size;
+			if (!buffer[itr].ready())
+			{
+				BOOST_LOG_TRIVIAL(warning) << "Buffer not ready, increasing size of buffer pool";
+				resize(buffer.size() + 1);
+			}			
+			return &buffer[itr];
 		}
 		Buffer<T, P1>* waitBack()
 		{
@@ -404,14 +411,24 @@ template<typename Data>
 			boost::recursive_mutex::scoped_lock lock(mtx);
 			if (size == 0)
 				return nullptr;
-			return &buffer[putItr++%size];
+			auto itr = putItr++%size;
+			if (!buffer[itr]->ready())
+			{
+				BOOST_LOG_TRIVIAL(warning) << "Buffer not ready, increasing size of buffer pool";
+				resize(buffer.size() + 1);
+			}
+			return &buffer[itr];
 		}
 		Buffer<T, P1, P2>* getBack()
 		{
 			boost::recursive_mutex::scoped_lock lock(mtx);
-			if (!buffer[getItr%size].ready())
-				return nullptr;
-			return &buffer[getItr++%size];
+			auto itr = getItr++%size;
+			if (!buffer[itr].ready())
+			{
+				BOOST_LOG_TRIVIAL(warning) << "Buffer not ready, increasing size of buffer pool";
+				resize(buffer.size() + 1);
+			}
+			return &buffer[itr];
 		}
 		Buffer<T, P1, P2>* waitBack()
 		{
