107c8b77b91341f886d6d9d4fa3f07df462b5b9a
Dan Moodie <dtmoodie@gmail.com>
2015-11-25 17:09:04 -0500

    Callbacks seem to work but not as fast as I would like.

--- EagleLib/src/EagleLib/utilities/CudaCallbacks.cpp
+++ EagleLib/src/EagleLib/utilities/CudaCallbacks.cpp
@@ -1,9 +1,37 @@
 #include "CudaCallbacks.hpp"
-
+#include <boost/log/trivial.hpp>
+using namespace EagleLib::cuda;
 
 void EagleLib::cuda::ICallback::cb_func(int status, void* user_data)
 {
 	auto cb = static_cast<ICallback*>(user_data);
-	cb->run();
-	delete cb;
+	pplx::create_task([cb]()->void
+	{
+		auto start = clock();
+		cb->run();
+		BOOST_LOG_TRIVIAL(info) << "Callback execution time: " << clock() - start;
+		delete cb;
+	});	
+}
+scoped_event_timer::scoped_event_timer(cv::cuda::Stream& stream, const std::string& scope_name)
+{
+	_stream = stream;
+	_scope_name = scope_name;
+	_start.record(stream);
+}
+scoped_event_timer::~scoped_event_timer()
+{
+	_end.record(_stream);
+	cv::cuda::Event end = _end;
+	cv::cuda::Event start = _start;
+	cv::cuda::Stream stream = _stream;
+	std::string name = _scope_name;
+	pplx::create_task(
+		[start, end, stream, name]()->void
+	{
+		cv::cuda::Event ev = end; // end is capture as a const value, need to make a copy to get rid of const
+		ev.waitForCompletion();
+		BOOST_LOG_TRIVIAL(info) << "[" << name << "] ellapsed time " << 
+			cv::cuda::Event::elapsedTime(start, end);
+	});	
 }
--- EagleLib/src/EagleLib/utilities/CudaCallbacks.hpp
+++ EagleLib/src/EagleLib/utilities/CudaCallbacks.hpp
@@ -1,22 +1,61 @@
 #pragma once
+#include "EagleLib/Defs.hpp"
 #include <opencv2/core/cuda.hpp>
+#include <functional>
+#include <future>
+
 #include <pplx/pplxtasks.h>
 
 namespace EagleLib
 {
 	namespace cuda
 	{
-		struct ICallback
+
+		struct EAGLE_EXPORTS scoped_event_timer
+		{
+			std::string _scope_name;
+			cv::cuda::Stream _stream;
+			cv::cuda::Event _start;
+			cv::cuda::Event _end;
+			scoped_event_timer(cv::cuda::Stream& stream, const std::string& scope_name = "");
+			~scoped_event_timer();
+		};
+		struct EAGLE_EXPORTS ICallback
 		{
 			static void cb_func(int status, void* user_data);
 			virtual void run() = 0;
 		};
 		
-		template<typename T, typename C> void enqueue_callback(const T& user_data, cv::cuda::Stream& stream)
+		template<typename T, typename C> 
+		auto enqueue_callback(
+			const T& user_data, 
+			cv::cuda::Stream& stream) -> void
 		{
-			static_assert(std::is_base_of<ICallback, C>::value);
-			stream.enqueueHostCallback(ICallback::cb_func, new C(T));
+			static_assert(std::is_base_of<ICallback, C>::value, "Template class argument must inherit from ICallback");
+			stream.enqueueHostCallback(ICallback::cb_func, new C(user_data));
 		}
+
+		
+		
+		template<typename _return_type> 
+		struct FunctionCallback: public ICallback
+		{
+			std::function<_return_type()> func;
+			std::promise<_return_type> promise;
+			FunctionCallback(const std::function<_return_type()>& f) : func(f) {}
+			virtual void run();
+		};
+
+
+		template<typename _Ty> auto
+			enqueue_callback(_Ty function,cv::cuda::Stream& stream)->std::future<typename pplx::details::_TaskTypeFromParam<_Ty>::_Type>
+		{
+			auto fc = new FunctionCallback<typename pplx::details::_TaskTypeFromParam<_Ty>::_Type>(function);
+			stream.enqueueHostCallback(&ICallback::cb_func, fc);
+			return fc->promise.get_future();
+		}
+		
+
 
 		template<typename T> class Callback: public ICallback
 		{
@@ -26,6 +65,10 @@ namespace EagleLib
 
 
 		// Implementations
+		template<typename T> void FunctionCallback<T>::run()
+		{
+			promise.set_value(func());
+		}
 
 		template<typename T> void Callback<T>::run()
 		{
