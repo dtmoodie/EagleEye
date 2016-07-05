85950c3017e04a7101a37c45904c258458bb42e1
dan <dtmoodie@gmail.com>
2016-07-05 14:54:15 -0500

    Start of volumetric rendering.

---
+++ Plugins/vtkRendering/src/vtkVolumetricPlotter.cpp
@@ -0,0 +1,155 @@
+#include "vtkVolumetricPlotter.h"
+#include <EagleLib/SyncedMemory.h>
+#include <vtkDataReader.h>
+#include <vtkStructuredPoints.h>
+#include <vtkErrorCode.h>
+#include <vtkSmartVolumeMapper.h>
+#include <vtkPointData.h>
+#include <vtkColorTransferFunction.h>
+#include <vtkVolumeProperty.h>
+#include <vtkUnsignedShortArray.h>
+
+using namespace EagleLib;
+using namespace EagleLib::Plotting;
+
+bool vtkVolumetricPlotterInfo::AcceptsParameter(Parameters::Parameter* param)
+{
+    auto type = param->GetTypeInfo();
+    if(type == Loki::TypeInfo(typeid(TS<SyncedMemory>)))
+    {
+        auto typed = dynamic_cast<Parameters::ITypedParameter<TS<SyncedMemory>>*>(param);
+        if(typed)
+        {
+            auto ts_data = typed->Data();
+            if(ts_data)
+            {
+                auto shape = ts_data->GetShape();
+                if(shape.size() == 4)
+                {
+                    if(shape[0] == 1 /*channels are packed in last dim */ || shape[3] == 1 /*channels are packed in first dim */)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+
+std::string vtkVolumetricPlotterInfo::GetObjectName()
+{
+    return "vtkVolumetricPlotter";
+}
+
+std::string vtkVolumetricPlotterInfo::GetObjectTooltip()
+{
+    return "Plot data as if each channel is along the Z axis";
+}
+
+std::string vtkVolumetricPlotterInfo::GetObjectHelp()
+{
+    return "Volumetric plot .....";
+}
+
+static vtkVolumetricPlotterInfo g_volumeInfo;
+
+
+vtkVolumetricPlotter::~vtkVolumetricPlotter()
+{
+}
+bool vtkVolumetricPlotter::AcceptsParameter(Parameters::Parameter* param)
+{
+    return g_volumeInfo.AcceptsParameter(param);
+}
+void vtkVolumetricPlotter::SetInput(Parameters::Parameter* param_)
+{
+    if(param_)
+    {
+        auto type = param_->GetTypeInfo();
+        if(type == Loki::TypeInfo(typeid(TS<SyncedMemory>)))
+        {
+            vtkPlotter::SetInput(param_);
+            _connections[&param_->update_signal] = param_->update_signal.connect(std::bind(&vtkVolumetricPlotter::OnSyncedMemUpdate, this, std::placeholders::_1));
+            auto typed = dynamic_cast<Parameters::ITypedParameter<TS<SyncedMemory>>*>(param_);
+            if(typed && typed->Data())
+            {
+                //vtkSmartPointer<vtkSmartVolumeMapper> volumeMapper = vtkSmartPointer<vtkSmartVolumeMapper>::New();
+                auto data = typed->Data();
+                auto shape = data->GetShape();
+                int extent[6];
+                int dim_count = 0;
+                
+                for(int i = 0; i < shape.size(); ++i)
+                {
+                    if(shape[i] != 1)
+                    {
+                        extent[2*dim_count] = 0;
+                        extent[2*dim_count+1] = shape[i];
+                        ++dim_count;
+                    }
+                }
+                size_t size = extent[1] * extent[3] * extent[5];
+
+                vtkSmartPointer<vtkStructuredPoints> dataset = vtkSmartPointer<vtkStructuredPoints>::New();
+                dataset->SetExtent(extent);
+                dataset->SetOrigin(0,0,0);
+                dataset->SetSpacing(0,0,0);
+                auto points = dataset->GetPointData();
+                vtkUnsignedShortArray *scalars = vtkUnsignedShortArray::New();
+                
+                scalars->SetNumberOfComponents(1);
+                scalars->SetNumberOfValues(size);
+                size_t channel_stride = extent[3]*extent[5];
+                if(shape[0] != 1 && shape[3] == 1)
+                {
+                    for(int c = 0; c < shape[0]; ++c)
+                    {
+                        cv::Mat wrapped(extent[3], extent[5], CV_16U, scalars->GetPointer(c*channel_stride));
+                        cv::Mat input = data->GetMat(cv::cuda::Stream::Null(), c);
+                        input.convertTo(wrapped, CV_16U);
+                    }
+                }
+                scalars->DataChanged();
+                points->SetScalars(scalars);
+                vtkSmartPointer<vtkSmartVolumeMapper> mapper = vtkSmartPointer<vtkSmartVolumeMapper>::New();
+                mapper->SetInputData(dataset);
+
+                vtkSmartPointer<vtkVolumeProperty> volumeProperty = vtkSmartPointer<vtkVolumeProperty>::New();
+                volumeProperty->ShadeOff();
+                volumeProperty->SetInterpolationType(VTK_LINEAR_INTERPOLATION);
+
+
+                vtkSmartPointer<vtkColorTransferFunction> color =  vtkSmartPointer<vtkColorTransferFunction>::New();
+                color->AddRGBPoint(0,    0.0, 0.0, 1.0);
+                color->AddRGBPoint(1024, 1.0, 0.0, 0.0);
+                color->AddRGBPoint(2048, 1.0, 1.0, 1.0);
+                volumeProperty->SetColor(color);
+
+                vtkSmartPointer<vtkVolume> volume = vtkSmartPointer<vtkVolume>::New();
+                volume->SetMapper(mapper);
+                volume->SetProperty(volumeProperty);
+                renderer->AddViewProp(volume);
+            }
+        }
+    }
+}
+void vtkVolumetricPlotter::OnParameterUpdate(cv::cuda::Stream* stream)
+{
+}
+void vtkVolumetricPlotter::OnMatParameterUpdate(cv::cuda::Stream* stream)
+{
+}
+void vtkVolumetricPlotter::OnGpuMatParameterUpdate(cv::cuda::Stream* stream)
+{
+}
+void vtkVolumetricPlotter::OnSyncedMemUpdate(cv::cuda::Stream* stream)
+{
+}
+std::string vtkVolumetricPlotter::PlotName() const
+{
+    return "vtkVolumetricPlotter";
+}
+
+REGISTERCLASS(vtkVolumetricPlotter, &g_volumeInfo)
+
---
+++ Plugins/vtkRendering/src/vtkVolumetricPlotter.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "vtkPlotter.h"
+#include "vtkMatDataBuffer.h"
+
+#include <vtkPolyData.h>
+#include <vtkIdTypeArray.h>
+#include <vtkLODActor.h>
+#ifdef _MSC_VER
+#ifdef _DEBUG
+RUNTIME_COMPILER_LINKLIBRARY("vtkRenderingVolume-" VTK_VERSION_ "d.lib")
+RUNTIME_COMPILER_LINKLIBRARY("vtkRenderingVolumeOpenGL2-" VTK_VERSION_ "d.lib")
+#else
+RUNTIME_COMPILER_LINKLIBRARY("vtkRenderingVolume-" VTK_VERSION_ ".lib")
+#endif
+#else
+
+#endif
+namespace EagleLib
+{
+    namespace Plotting
+    {
+        struct PLUGIN_EXPORTS vtkVolumetricPlotterInfo: public vtkPlotterInfo
+        {
+            virtual bool AcceptsParameter(Parameters::Parameter* param);
+            virtual std::string GetObjectName();
+            virtual std::string GetObjectTooltip();
+            virtual std::string GetObjectHelp();
+        };
+
+        class PLUGIN_EXPORTS vtkVolumetricPlotter : public vtkPlotter
+        {
+        public:
+            virtual ~vtkVolumetricPlotter();
+            virtual bool AcceptsParameter(Parameters::Parameter* param);
+            virtual void SetInput(Parameters::Parameter* param_ = nullptr);
+            virtual void OnParameterUpdate(cv::cuda::Stream* stream);
+            virtual void OnMatParameterUpdate(cv::cuda::Stream* stream);
+            virtual void OnGpuMatParameterUpdate(cv::cuda::Stream* stream);
+            virtual void OnSyncedMemUpdate(cv::cuda::Stream* stream);
+            virtual std::string PlotName() const;
+        };
+    }
+}
+
+
