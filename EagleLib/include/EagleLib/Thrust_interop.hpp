eea6bd9b8b077a3231ab1307a52c93c4de93a767
Dan <dtmoodie@gmail.com>
2016-11-29 21:31:39 -0500

    CMake find target cleanup, prep for mxnet.

--- EagleLib/include/EagleLib/Thrust_interop.hpp
+++ EagleLib/include/EagleLib/Thrust_interop.hpp
@@ -28,6 +28,220 @@ template<typename T> struct step_functor : public thrust::unary_function<int, in
         return idx;
     }
 };
+template<typename T, int CN, typename PtrType> class ThrustView;
+template<typename T, int CN> ThrustView<T, CN, T*> CreateView(cv::Mat& mat, int channel = 0);
+template<typename T, int CN> ThrustView<T, CN, thrust::device_ptr<T>> CreateView(cv::cuda::GpuMat& mat, int channel = 0);
+template<typename T, int CN> ThrustView<const T, CN, T*> CreateView(const cv::Mat& mat, int channel = 0);
+template<typename T, int CN> ThrustView<const T, CN, thrust::device_ptr<T>> CreateView(const cv::cuda::GpuMat& mat, int channel = 0);
+
+
+template<typename T, int CN, typename PtrType = thrust::device_ptr<T>> class ThrustView
+{
+    // Iterates over every element inside of a matrix
+    typedef cv::Vec<T, CN> DataType;
+    typedef thrust::permutation_iterator<PtrType, thrust::transform_iterator<step_functor<T>, thrust::counting_iterator<int>>> MatrixItr_t;
+    typedef thrust::permutation_iterator<PtrType, thrust::counting_iterator<int>> RowItr_t;
+    // Outer iterator iterates over each row of the matrix, inner iterator iterats over elemetns of the row
+    typedef thrust::permutation_iterator<RowItr_t, thrust::transform_iterator<step_functor<T>, thrust::counting_iterator<int>>> RowsItr_t;
+    typedef MatrixItr_t ColItr_t;
+};
+
+template<typename T, typename PtrType> class ThrustView<T, 1, PtrType>
+{
+public:
+    // Iterates over every element inside of a matrix
+    typedef thrust::permutation_iterator<PtrType, thrust::transform_iterator<step_functor<T>, thrust::counting_iterator<int>>> MatrixItr_t;
+    typedef thrust::permutation_iterator<PtrType, thrust::counting_iterator<int>> RowItr_t;
+    typedef MatrixItr_t ColItr_t;
+    // Outer iterator iterates over each row of the matrix, inner iterator iterats over elemetns of the row
+    typedef thrust::permutation_iterator<thrust::zip_iterator<thrust::tuple<RowItr_t, RowItr_t>>, thrust::transform_iterator<step_functor<T>, thrust::counting_iterator<int>>> RowsItr_t;
+    typedef thrust::permutation_iterator<thrust::zip_iterator<thrust::tuple<ColItr_t, ColItr_t>>, thrust::transform_iterator<step_functor<T>, thrust::counting_iterator<int>>> ColsItr_t;
+
+    MatrixItr_t begin()
+    {
+        return thrust::make_permutation_iterator(PtrType(_data),
+                    thrust::make_transform_iterator(thrust::make_counting_iterator(0), 
+                    step_functor<T>(_cols, _row_step, _element_step)));
+    }
+    MatrixItr_t end()
+    {
+        return thrust::make_permutation_iterator(PtrType(_data),
+                    thrust::make_transform_iterator(thrust::make_counting_iterator(_rows*_cols), 
+                    step_functor<T>(_cols, _row_step, _element_step)));
+    }
+
+    MatrixItr_t diagBegin()
+    {
+        return thrust::make_permutation_iterator(PtrType(_data),
+            thrust::make_transform_iterator(thrust::make_counting_iterator(0), 
+                step_functor<T>(1, 1 + _row_step, _element_step)));
+    }
+    MatrixItr_t diagEndItr()
+    {
+        return thrust::make_permutation_iterator(PtrType(_data),
+            thrust::make_transform_iterator(thrust::make_counting_iterator(std::min(_rows, _cols)), step_functor<T>(1, 1 + _row_step, _element_step)));
+    }
+
+    ColItr_t colBegin(const int col)
+    {
+        return thrust::make_permutation_iterator(PtrType(_data),
+            thrust::make_transform_iterator(thrust::make_counting_iterator(0),
+                step_functor<T>(1, _row_step, _element_step)));
+    }
+    ColItr_t colEnd(const int col)
+    {
+        return thrust::make_permutation_iterator(PtrType(_data),
+            thrust::make_transform_iterator(thrust::make_counting_iterator(_cols),
+                step_functor<T>(1, _row_step, _element_step)));
+    }
+
+    RowItr_t rowBegin(const int row)
+    {
+        return thrust::make_permutation_iterator(PtrType(ptr(row)),
+                    thrust::make_counting_iterator(0));
+    }
+    RowItr_t rowEnd(const int row)
+    {
+        return thrust::make_permutation_iterator(PtrType(ptr(row)),
+                thrust::make_counting_iterator(_cols));
+    }
+
+    std::pair<RowsItr_t, RowsItr_t> rowRange(int start_row = 0, int end_row = -1)
+    {
+        if(end_row == -1)
+            end_row = _rows;
+        auto begin_itr = rowBegin(start_row);
+        auto zip = thrust::make_zip_iterator(thrust::make_tuple(begin_itr, begin_itr));
+
+        return std::make_pair(thrust::make_permutation_iterator(zip,
+                    thrust::make_transform_iterator(thrust::make_counting_iterator(0),
+                        step_functor<T>(1, _row_step, _element_step))),
+                    thrust::make_permutation_iterator(zip,
+                        thrust::make_transform_iterator(thrust::make_counting_iterator(end_row - start_row),
+                        step_functor<T>(1, _row_step, _element_step))));
+    }
+    // WIP
+    std::pair<ColsItr_t, ColsItr_t> colRange(int start_col = 0, int end_col = -1)
+    {
+        if(end_col == -1)
+            end_col = _cols;
+        auto begin_itr = colBegin(start_col);
+        auto end_itr = colEnd(start_col);
+        auto zip = thrust::make_zip_iterator(thrust::make_tuple(begin_itr, end_itr));
+
+        return std::make_pair(thrust::make_permutation_iterator(zip,
+            thrust::make_transform_iterator(thrust::make_counting_iterator(0),
+                step_functor<T>(1, 1, _element_step))),
+            thrust::make_permutation_iterator(zip,
+                thrust::make_transform_iterator(thrust::make_counting_iterator(end_col - start_col),
+                    step_functor<T>(1, 1, _element_step))));
+    }
+
+    
+    T* ptr(int row = 0)
+    {
+        return _data + _row_step * row;
+    }
+    T* _data;
+    int _rows;
+    int _cols;
+    // Num elements per row, accounts for padding
+    int _row_step;
+    int _element_step;
+};
+
+template<typename T, int CN> ThrustView<T, CN, T*> CreateView(cv::Mat& mat, int channel)
+{
+    ThrustView<T, CN, T*> view;
+    CV_Assert(mat.depth() == cv::DataType<T>::depth);
+    view._rows = mat.rows;
+    view._cols = mat.cols;
+    // Mat's step is stored in bytes
+    view._row_step = mat.step / sizeof(T);
+    if (channel == -1)
+    {
+        // Virtually reshape the matrix such that each channel of the image is seen as a separate adjacent pixel
+        // IE [(RGB),(RGB)] -> [R,G,B,R,G,B]
+        view._element_step = 1;
+        view._data = (T*)mat.ptr();
+    }
+    else
+    {
+        view._element_step = mat.channels();
+        view._data = (T*)(mat.ptr() + channel);
+    }
+    return view;
+}
+
+template<typename T, int CN> ThrustView<const T, CN, T*> CreateView(const cv::Mat& mat, int channel)
+{
+    ThrustView<T, CN, T*> view;
+    CV_Assert(mat.depth() == cv::DataType<T>::depth);
+    view._rows = mat.rows;
+    view._cols = mat.cols;
+    // Mat's step is stored in bytes
+    view._row_step = mat.step / sizeof(T);
+    if (channel == -1)
+    {
+        // Virtually reshape the matrix such that each channel of the image is seen as a separate adjacent pixel
+        // IE [(RGB),(RGB)] -> [R,G,B,R,G,B]
+        view._element_step = 1;
+        view._data = (T*)mat.ptr();
+    }
+    else
+    {
+        view._element_step = mat.channels();
+        view._data = (T*)(mat.ptr() + channel);
+    }
+    return view;
+}
+
+
+template<typename T, int CN> ThrustView<T, CN, thrust::device_ptr<T>> CreateView(cv::cuda::GpuMat& mat, int channel)
+{
+    ThrustView<T, CN, thrust::device_ptr<T>> view;
+    CV_Assert(mat.depth() == cv::DataType<T>::depth);
+    view._rows = mat.rows;
+    view._cols = mat.cols;
+    // Mat's step is stored in bytes
+    view._row_step = mat.step / sizeof(T);
+    if (channel == -1)
+    {
+        // Virtually reshape the matrix such that each channel of the image is seen as a separate adjacent pixel
+        // IE [(RGB),(RGB)] -> [R,G,B,R,G,B]
+        view._element_step = 1;
+        view._data = (T*)mat.ptr();
+    }
+    else
+    {
+        view._element_step = mat.channels();
+        view._data = (T*)(mat.ptr() + channel);
+    }
+    return view;
+}
+
+template<typename T, int CN> ThrustView<const T, CN, thrust::device_ptr<T>> CreateView(const cv::cuda::GpuMat& mat, int channel)
+{
+    ThrustView<const T, CN, thrust::device_ptr<T>> view;
+    CV_Assert(mat.depth() == cv::DataType<T>::depth);
+    view._rows = mat.rows;
+    view._cols = mat.cols;
+    // Mat's step is stored in bytes
+    view._row_step = mat.step / sizeof(T);
+    if (channel == -1)
+    {
+        // Virtually reshape the matrix such that each channel of the image is seen as a separate adjacent pixel
+        // IE [(RGB),(RGB)] -> [R,G,B,R,G,B]
+        view._element_step = 1;
+        view._data = mat.ptr();
+    }
+    else
+    {
+        view._element_step = mat.channels();
+        view._data = mat.ptr() + channel;
+    }
+    return view;
+}
 
 /*
 @Brief GpuMatBeginItr returns a thrust compatible iterator to the beginning of a GPU mat's memory.
