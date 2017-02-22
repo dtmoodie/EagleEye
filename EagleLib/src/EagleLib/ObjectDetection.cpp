523b91a1b8362019756f5973e26e282f065f42ea
dan <dtmoodie@gmail.com>
2017-02-22 09:15:12 -0500

    preliminary 3d object detection

--- EagleLib/src/EagleLib/ObjectDetection.cpp
+++ EagleLib/src/EagleLib/ObjectDetection.cpp
@@ -1,4 +1,5 @@
 #include "EagleLib/ObjectDetection.hpp"
+#include <EagleLib/rcc/external_includes/cv_imgproc.hpp>
 #include <cereal/types/vector.hpp>
 #include <cereal/cereal.hpp>
 #include <cereal/types/string.hpp>
@@ -9,6 +10,7 @@
 #include "MetaObject/Parameters/Buffers/CircularBuffer.hpp"
 #include "MetaObject/Parameters/Buffers/StreamBuffer.hpp"
 #include "MetaObject/Parameters/Buffers/map.hpp"
+#include "MetaObject/Parameters/Buffers/NNStreamBuffer.hpp"
 #include "MetaObject/Parameters/IO/CerealPolicy.hpp"
 //#include "MetaObject/Parameters/IO/TextPolicy.hpp"
 using namespace EagleLib;
@@ -19,24 +21,20 @@ EagleLib::Classification::Classification(const std::string& label_, float confid
 
 }
 
-namespace cereal
+void EagleLib::CreateColormap(cv::Mat& lut, int num_classes, int ignore_class)
 {
-    template<class AR, class T> void serialize(AR& ar, cv::Rect_<T>& rect)
-    {
-        ar(make_nvp("x", rect.x), make_nvp("y", rect.y), make_nvp("width", rect.width), make_nvp("height", rect.height));
-    }
+    lut.create(1, num_classes, CV_8UC3);
+    for(int i = 0; i < num_classes; ++i)
+        lut.at<cv::Vec3b>(i) = cv::Vec3b(i*180 / num_classes, 200, 255);
+    cv::cvtColor(lut, lut, cv::COLOR_HSV2BGR);
+    if(ignore_class != -1 && ignore_class < num_classes)
+        lut.at<cv::Vec3b>(ignore_class) = cv::Vec3b(0,0,0);
 }
 
-template<class AR> void Classification::serialize(AR& ar)
-{
-    ar(CEREAL_NVP(label), CEREAL_NVP(confidence), CEREAL_NVP(classNumber));
-}
+INSTANTIATE_META_PARAMETER(DetectedObject)
+INSTANTIATE_META_PARAMETER(Classification)
+INSTANTIATE_META_PARAMETER(std::vector<DetectedObject>)
 
-template<class AR> void DetectedObject::serialize(AR& ar)
-{
-    ar(CEREAL_NVP(boundingBox), CEREAL_NVP(detections));
-}
+template EAGLE_EXPORTS void DetectedObject::serialize<cereal::JSONInputArchive>(cereal::JSONInputArchive& ar);
+template EAGLE_EXPORTS void DetectedObject::serialize<cereal::JSONOutputArchive>(cereal::JSONOutputArchive& ar);
 
-INSTANTIATE_META_PARAMETER(DetectedObject);
-INSTANTIATE_META_PARAMETER(Classification);
-INSTANTIATE_META_PARAMETER(std::vector<DetectedObject>);
