ebdc211fea1043998ae59253cf25c1aa1ce5d5f2
dan <dtmoodie@gmail.com>
2017-02-28 20:52:04 -0500

    logging directory

--- EagleLib/include/EagleLib/Logging.h
+++ EagleLib/include/EagleLib/Logging.h
@@ -5,6 +5,6 @@
 //#define LOG(severity) BOOST_LOG(severity) << "[" << __FUNCTION__ << "] " 
 namespace EagleLib
 {
-    void EAGLE_EXPORTS SetupLogging();
+    void EAGLE_EXPORTS SetupLogging(const std::string& log_dir = "");
     void EAGLE_EXPORTS ShutdownLogging();
 }
