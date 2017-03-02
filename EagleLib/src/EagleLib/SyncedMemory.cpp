1e1699a09ad09d42277f55e664a95811614db1bc
dan <dtmoodie@gmail.com>
2017-03-02 00:48:46 -0500

    Windows compilation fixes.

--- EagleLib/src/EagleLib/SyncedMemory.cpp
+++ EagleLib/src/EagleLib/SyncedMemory.cpp
@@ -523,3 +523,5 @@ void TypedInputParameterPtr<SyncedMemory>::onInputDelete(IParameter const* param
         this->input = nullptr;
     }
 }
+
+TEMPLATE_EXTERN template class EAGLE_EXPORTS TypedInputParameterPtr<SyncedMemory>;
