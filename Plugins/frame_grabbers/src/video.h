6fcea91f5e4d95c0de8a26d05d1a218814aea3d5
dan <dtmoodie@gmail.com>
2017-03-09 13:47:40 +0000

    frame grabbers plugin bug fixes

--- Plugins/frame_grabbers/src/video.h
+++ Plugins/frame_grabbers/src/video.h
@@ -6,7 +6,7 @@ namespace EagleLib
 {
     namespace Nodes
     {
-    class PLUGIN_EXPORTS frame_grabber_video : public frame_grabber_cv
+    class frame_grabber_video : public frame_grabber_cv
     {
     public:
         ~frame_grabber_video();
