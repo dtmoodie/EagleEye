c7aeff71fdb8e7d4a1dcaad0433be9d0d7018f94
dan <dtmoodie@gmail.com>
2017-03-20 09:08:57 -0400

    make video writer use a traditional thread

--- Plugins/Core/src/IO/VideoWriter.h
+++ Plugins/Core/src/IO/VideoWriter.h
@@ -6,6 +6,7 @@
 #include "RuntimeSourceDependency.h"
 #include "MetaObject/Thread/ThreadHandle.hpp"
 #include "MetaObject/Thread/ThreadPool.hpp"
+#include "MetaObject/Detail/ConcurrentQueue.hpp"
 RUNTIME_COMPILER_SOURCEDEPENDENCY
 RUNTIME_MODIFIABLE_INCLUDE
 namespace aq
@@ -16,6 +17,7 @@ namespace Nodes
     class VideoWriter : public Node
     {
     public:
+        ~VideoWriter();
         MO_DERIVE(VideoWriter, Node)
             INPUT(SyncedMemory, image, nullptr)
             PROPERTY(cv::Ptr<cv::cudacodec::VideoWriter>, d_writer, cv::Ptr<cv::cudacodec::VideoWriter>())
@@ -24,11 +26,13 @@ namespace Nodes
             PARAM(mo::WriteFile, filename, mo::WriteFile("video.avi"))
             PARAM(bool, using_gpu_writer, true)
             MO_SLOT(void, write_out)
-            PROPERTY(mo::ThreadHandle, _write_thread, mo::ThreadPool::Instance()->RequestThread())
+            //PROPERTY(mo::ThreadHandle, _write_thread, mo::ThreadPool::Instance()->RequestThread())
         MO_END;
         void NodeInit(bool firstInit);
     protected:
         bool ProcessImpl();
+        boost::thread _write_thread;
+        moodycamel::ConcurrentQueue<cv::Mat> _write_queue;
     };
 #ifdef HAVE_FFMPEG
     class VideoWriterFFMPEG: public Node
