8f3a1427f085f46857f96261c85806201eff3af1
dan <dtmoodie@gmail.com>
2017-03-21 23:01:42 -0400

    fix build on windows.

--- Plugins/point_clouds/include/pc_export.h
+++ Plugins/point_clouds/include/pc_export.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "RuntimeLinkLibrary.h"
 #if (defined WIN32 || defined _WIN32 || defined WINCE || defined __CYGWIN__) && defined point_clouds_EXPORTS
 #define PC_EXPORTS __declspec(dllexport)
 #ifdef _DEBUG
