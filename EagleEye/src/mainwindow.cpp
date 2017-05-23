3be85fe62431651afc714c501d8abf53c69d2613
dan <dtmoodie@gmail.com>
2017-05-23 14:26:27 -0400

    WIP nodeeditor

--- EagleEye/src/mainwindow.cpp
+++ EagleEye/src/mainwindow.cpp
@@ -9,10 +9,23 @@
 #include <RuntimeObjectSystem/ObjectInterface.h>
 #include <IObject.h>
 namespace aq{
-    class ParamOutProxy: virtual public QtNodes::NodeData{
+    class NodeOutProxy: virtual public QtNodes::NodeData{
+    public:
+        NodeOutProxy(const rcc::shared_ptr<Nodes::Node>& node_):
+            node(node_){}
+        virtual QtNodes::NodeDataType type() const{
+            QtNodes::NodeDataType out;
+            out.name = QString::fromStdString(node->GetTreeName());
+            out.id = QString::fromStdString("aq::Nodes::Node");
+            return out;
+        }
+        rcc::shared_ptr<Nodes::Node> node;
+    };
+
+    class ParamOutProxy: virtual public NodeOutProxy{
     public:
         ParamOutProxy(mo::IParameter* param_, const rcc::shared_ptr<Nodes::Node>& node_):
-        param(param_), node(node_){
+        param(param_), NodeOutProxy(node_){
         }
         virtual QtNodes::NodeDataType type() const{
             QtNodes::NodeDataType out;
@@ -21,7 +34,6 @@ namespace aq{
             return out;
         }
         mo::IParameter* param;
-        rcc::shared_ptr<Nodes::Node> node;
     };
 
     class NodeProxy: virtual public QtNodes::NodeDataModel{
@@ -39,46 +51,54 @@ namespace aq{
             return std::unique_ptr<QtNodes::NodeDataModel>(new NodeProxy(std::move(node)));
         }
 
-        virtual
-        unsigned int nPorts(QtNodes::PortType portType) const{
-            switch(portType)
-            {
-                case QtNodes::PortType::In: return m_obj->GetInputs().size();
-                case QtNodes::PortType::Out: return m_obj->GetOutputs().size();
+        virtual unsigned int nPorts(QtNodes::PortType portType) const{
+            switch(portType){
+                case QtNodes::PortType::In: return m_obj->GetInputs().size() + 1;
+                case QtNodes::PortType::Out: return m_obj->GetOutputs().size() + 1;
             }
-
             return 0;
         }
 
-        virtual
-        QtNodes::NodeDataType dataType(QtNodes::PortType portType, QtNodes::PortIndex portIndex) const{
+        virtual QtNodes::NodeDataType dataType(QtNodes::PortType portType, QtNodes::PortIndex portIndex) const{
             QtNodes::NodeDataType output;
-            switch(portType)
-            {
+            switch(portType){
                 case QtNodes::PortType::In: {
-                    auto inputs = m_obj->GetInputs();
-                    if(portIndex < inputs.size() && portIndex >=0){
-                        output.id = QString::fromStdString(inputs[portIndex]->GetTypeInfo().name());
-                        output.name = QString::fromStdString(inputs[portIndex]->GetName());
+                    if(portIndex == 0){
+                        output.id = "aq::Nodes::Node";
+                        output.name = "parents";
+                    }else{
+                        auto inputs = m_obj->GetInputs();
+                        if(portIndex <= inputs.size() && portIndex > 0){
+                            output.id = QString::fromStdString(inputs[portIndex - 1]->GetTypeInfo().name());
+                            output.name = QString::fromStdString(inputs[portIndex - 1]->GetName());
+                        }
                     }
                     break;
                 }
                 case QtNodes::PortType::Out: {
-                    auto outputs = m_obj->GetOutputs();
-                    if(portIndex >= 0 && portIndex < outputs.size()){
-                        output.id = QString::fromStdString(outputs[portIndex]->GetTypeInfo().name());
-                        output.name = QString::fromStdString(outputs[portIndex]->GetName());
+                    if(portIndex == 0){
+                        output.id = "aq::Nodes::Node";
+                        output.name = "children";
+                    }else{
+                        auto outputs = m_obj->GetOutputs();
+                        if(portIndex > 0 && portIndex <= outputs.size()){
+                            output.id = QString::fromStdString(outputs[portIndex - 1]->GetTypeInfo().name());
+                            output.name = QString::fromStdString(outputs[portIndex - 1]->GetName());
+                        }
                     }
                 }
             }
             return output;
         }
 
-        virtual
-        void
-        setInData(std::shared_ptr<QtNodes::NodeData> nodeData,
-                  QtNodes::PortIndex port){
-            // TODO
+        virtual void setInData(std::shared_ptr<QtNodes::NodeData> nodeData, QtNodes::PortIndex port){
+            if(port == 0){
+                std::shared_ptr<NodeOutProxy> typed = std::dynamic_pointer_cast<NodeOutProxy>(nodeData);
+                if(typed){
+                    m_obj->AddParent(typed->node.Get());
+                    return;
+                }
+            }
             std::shared_ptr<ParamOutProxy> typed = std::dynamic_pointer_cast<ParamOutProxy>(nodeData);
             if(typed){
                 auto inputs = m_obj->GetInputs();
@@ -88,23 +108,59 @@ namespace aq{
             }
         }
 
-        virtual
-        std::shared_ptr<QtNodes::NodeData>
-        outData(QtNodes::PortIndex port){
+        virtual std::shared_ptr<QtNodes::NodeData> outData(QtNodes::PortIndex port){
+            if(port == 0){
+                return std::make_shared<NodeOutProxy>(m_obj);
+            }
             auto outputs = m_obj->GetOutputs();
-            if(port>= 0 && port < outputs.size()){
-                return std::make_shared<ParamOutProxy>(outputs[port], m_obj);
+            if(port > 0 && port <= outputs.size()){
+                return std::make_shared<ParamOutProxy>(outputs[port - 1], m_obj);
             }
             return std::shared_ptr<QtNodes::NodeData>();
         }
-        virtual
-        QWidget *
-        embeddedWidget(){
+        virtual QWidget * embeddedWidget(){
             // TODO
             return nullptr;
         }
 
         rcc::shared_ptr<aq::Nodes::Node> m_obj;
+    };
+
+    class DataStreamProxy: virtual public QtNodes::NodeDataModel{
+    public:
+        DataStreamProxy(rcc::shared_ptr<aq::IDataStream>&& ds = rcc::shared_ptr<aq::IDataStream>()):
+            m_obj(std::move(ds)){}
+
+        virtual QString caption() const{return "DataStream";}
+        virtual QString name() const{return "DataStream";}
+        virtual std::unique_ptr<QtNodes::NodeDataModel> clone() const{
+            return std::unique_ptr<DataStreamProxy>(new DataStreamProxy(std::move(aq::IDataStream::Create("", ""))));
+        }
+        virtual unsigned int nPorts(QtNodes::PortType portType) const{
+            if(portType == QtNodes::PortType::Out) return 1;
+            return 0;
+        }
+        virtual QtNodes::NodeDataType dataType(QtNodes::PortType port_type, QtNodes::PortIndex port_index) const{
+            QtNodes::NodeDataType output;
+            if(port_type == QtNodes::PortType::Out){
+                output.id = "aq::Nodes::Node";
+                output.name = "children";
+            }
+            return output;
+        }
+        virtual void setInData(std::shared_ptr<QtNodes::NodeData> nodeData, QtNodes::PortIndex port){
+
+        }
+        virtual std::shared_ptr<QtNodes::NodeData> outData(QtNodes::PortIndex port){
+
+            return std::shared_ptr<QtNodes::NodeData>();
+        }
+        virtual QWidget * embeddedWidget(){
+            // TODO
+            return nullptr;
+        }
+
+        rcc::shared_ptr<aq::IDataStream> m_obj;
     };
 
     class NodeConstructor: virtual public QtNodes::NodeDataModel{
@@ -162,7 +218,7 @@ MainWindow::MainWindow(QWidget *parent) :
         ids.push_back(ctr->GetInterfaceId());
         interfces_names.push_back(ctr->GetInterfaceName());
     }
-
+    registry->registerModel<aq::DataStreamProxy>("DataStream");
     ctrs = mo::MetaObjectFactory::Instance()->GetConstructors();
     for(auto ctr : ctrs){
         if(ctr->GetInterfaceName() == aq::Nodes::Node::GetInterfaceName())
