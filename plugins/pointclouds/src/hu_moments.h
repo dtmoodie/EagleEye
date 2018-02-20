cb7b2fd684a5f60f6cf83de59c5d68f5c0135675
dan <dtmoodie@gmail.com>
2018-02-19 21:47:33 -0500

    hu_moments bug fix, something wasn't committed correctly

--- plugins/pointclouds/src/hu_moments.h
+++ plugins/pointclouds/src/hu_moments.h
@@ -1,25 +1,23 @@
 #pragma once
+#include "aqpointclouds_export.hpp"
+#include <cereal/cereal.hpp>
 #include <ct/reflect/reflect_data.hpp>
-#include "aqpointcloudsExport.hpp"
 #include <opencv2/core/mat.hpp>
 #include <opencv2/core/types.hpp>
-#include <cereal/cereal.hpp>
 
 namespace point_clouds
 {
 
-class aqpointclouds_EXPORT Moment
-{
-public:
-    Moment(float Px_ = 0.0f, float Py_ = 0.0f, float Pz_ = 0.0f);
+    class aqpointclouds_EXPORT Moment
+    {
+      public:
+        Moment(float Px_ = 0.0f, float Py_ = 0.0f, float Pz_ = 0.0f);
 
-    float evaluate(cv::Mat mask, cv::Mat points, cv::Vec3f centroid);
-    REFLECT_INTERNAL_START(Moment)
-        REFLECT_INTERNAL_MEMBER(float, Px)
-        REFLECT_INTERNAL_MEMBER(float, Py)
-        REFLECT_INTERNAL_MEMBER(float, Pz)
-    REFLECT_INTERNAL_END;
-};
-
-
+        float evaluate(cv::Mat mask, cv::Mat points, cv::Vec3f centroid);
+        REFLECT_INTERNAL_START(Moment)
+            REFLECT_INTERNAL_MEMBER(float, Px)
+            REFLECT_INTERNAL_MEMBER(float, Py)
+            REFLECT_INTERNAL_MEMBER(float, Pz)
+        REFLECT_INTERNAL_END;
+    };
 }
