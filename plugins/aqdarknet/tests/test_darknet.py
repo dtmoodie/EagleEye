36576d450ef7b2103459f596f52bd368ae73d28c
dan <dtmoodie@gmail.com>
2020-11-05 04:51:19 +0000

    added more darknet test

--- plugins/aqdarknet/tests/test_darknet.py
+++ plugins/aqdarknet/tests/test_darknet.py
@@ -18,6 +18,29 @@ graph.setStream(stream)
 fg = aq.framegrabbers.create(args.path)
 graph.addNode(fg)
 
-#detector = aq.nodes.YOLO()
+detector = aq.nodes.YOLO(input=fg)
 
-#aq.eventLoop(100000)
+detector.weight_file = '/home/asiansensation/code/darknet/cfg/yolov3-tiny.weights'
+detector.model_file = '/home/asiansensation/code/darknet/cfg/yolov3-tiny.cfg'
+detector.label_file = '/home/asiansensation/code/darknet/data/coco.names'
+#detector.channel_mean = [0,0,0,0]
+#detector.pixel_scale = 1.0
+detector.det_thresh = 0.2
+detector.cat_thresh = 0.1
+
+draw = aq.nodes.DrawDetections(image=fg, detections=detector)
+writer = aq.nodes.ImageWriter(input_image=draw)
+writer.request_write = True
+
+print(detector)
+print('Weight file: \n{}'.format(detector.weight_file))
+
+aq.log('trace')
+graph.start()
+
+aq.eventLoop(10000)
+output = detector.output
+
+components = output.data.providers
+for component in components:
+    print(component.data)
