b050d3d177802f9e4c794be16d366ac20fce47ae
dan <dtmoodie@gmail.com>
2020-12-05 22:20:23 +0000

    load a folder and generate predictions for each image in the folder

--- plugins/aqdlib/tests/face_recognition_from_file.py
+++ plugins/aqdlib/tests/face_recognition_from_file.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 
-parser.add_argument('--path', default='maxresdefault.jpg')
+parser.add_argument('--path', default='images')
 parser.add_argument('--cfg', default='/home/asiansensation/code/yolo_tiny_face/yolov3-tiny.cfg')
 parser.add_argument('--weights', default='/home/asiansensation/code/yolo_tiny_face/yolov3-tiny_final.weights')
 parser.add_argument('--labels', default='/home/asiansensation/code/yolo_tiny_face/labels.txt')
@@ -38,6 +38,7 @@ draw = aq.nodes.DrawDetections(image=fg, detections=facedb)
 
 writer = aq.nodes.ImageWriter(input_image=draw)
 writer.request_write = True
+writer.frequency = 1
 
 graph.start()
 aq.eventLoop(10000)
@@ -48,5 +49,7 @@ components = output.data.components
 for component in components:
     print(component.data.data.typename)
     print(component.data.data.data)
+    if('Classification' in component.data.data.typename):
+        assert component.data.data.data[0].data[0].cat.data.name == 'JerryRyan'
 
-assert components[1].data.data.data[0].data[0].cat.data.name == 'JerryRyan'
+
