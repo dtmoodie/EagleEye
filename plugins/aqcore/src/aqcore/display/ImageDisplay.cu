af2986611c114c2d935de5e475c40cda08d5ea1b
dan <dtmoodie@gmail.com>
2021-03-04 20:31:16 -0500

    formatting

--- plugins/aqcore/src/aqcore/display/ImageDisplay.cu
+++ plugins/aqcore/src/aqcore/display/ImageDisplay.cu
@@ -1,46 +1,43 @@
 #include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <npp.h>
 #include <opencv2/core/cuda.hpp>
 #include <opencv2/cudev.hpp>
-#include <device_functions.h>
-#include <npp.h>
 
 namespace cv
 {
-namespace cuda
-{
-void drawHistogram(cv::InputArray histogram,
-                   cv::OutputArray draw,
-                   cv::InputArray bins = cv::noArray(),
-                   cv::cuda::Stream& stream = cv::cuda::Stream::Null());
+    namespace cuda
+    {
+        void drawHistogram(cv::InputArray histogram,
+                           cv::OutputArray draw,
+                           cv::InputArray bins = cv::noArray(),
+                           cv::cuda::Stream& stream = cv::cuda::Stream::Null());
 
-void drawPlot(cv::InputArray histogram,
-              cv::OutputArray draw,
-              cv::cuda::Stream& stream = cv::cuda::Stream::Null());
-}
-}
+        void
+        drawPlot(cv::InputArray histogram, cv::OutputArray draw, cv::cuda::Stream& stream = cv::cuda::Stream::Null());
+    } // namespace cuda
+} // namespace cv
 
-
-template<int N>
+template <int N>
 __global__ void draw_uchar_kernel(cv::cudev::PtrStepSz<cv::Vec3b> draw, const int* histogram, int size)
 {
 
     int x = blockIdx.x * blockDim.x + threadIdx.x;
 
-
     int max_bin = 0;
     int min_bin = NPP_MAX_32S;
     // first find the biggest bin
-    for(int i = 0; i < size * N; ++i)
+    for (int i = 0; i < size * N; ++i)
     {
         max_bin = max(max_bin, histogram[i]);
         min_bin = min(min_bin, histogram[i]);
     }
-    for(int i = 0; i < N; ++i)
+    for (int i = 0; i < N; ++i)
     {
         int value = histogram[x * N + i];
         float scaled = float(value - min_bin) / float(max_bin - min_bin);
         int start = draw.rows * (1 - scaled);
-        while(start < draw.rows)
+        while (start < draw.rows)
         {
             draw(start, x).val[i] = 255;
             ++start;
@@ -48,7 +45,7 @@ __global__ void draw_uchar_kernel(cv::cudev::PtrStepSz<cv::Vec3b> draw, const in
     }
 }
 
-template<class T, int N>
+template <class T, int N>
 __global__ void draw_kernel(cv::cudev::PtrStepSz<cv::Vec3b> draw, const T* histogram, int size)
 {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
@@ -56,18 +53,18 @@ __global__ void draw_kernel(cv::cudev::PtrStepSz<cv::Vec3b> draw, const T* histo
     T max_bin = histogram[0];
     T min_bin = histogram[0];
     // first find the biggest bin
-    for(int i = 1; i < size * N; ++i)
+    for (int i = 1; i < size * N; ++i)
     {
         max_bin = max(max_bin, histogram[i]);
         min_bin = min(min_bin, histogram[i]);
     }
-    for(int i = 0; i < N; ++i)
+    for (int i = 0; i < N; ++i)
     {
         float value = histogram[x * N + i];
         float scaled = float(value - min_bin) / float(max_bin - min_bin);
         int start = draw.rows * (1 - scaled);
         start = min(draw.rows - 1, max(0, start));
-        while(start < draw.rows)
+        while (start < draw.rows)
         {
             draw(start, x).val[i] = 255;
             ++start;
@@ -75,16 +72,16 @@ __global__ void draw_kernel(cv::cudev::PtrStepSz<cv::Vec3b> draw, const T* histo
     }
 }
 
-template<class T>
+template <class T>
 __global__ void draw_kernel(cv::cudev::PtrStepSz<cv::Vec3b> draw, const T* histogram, int size)
 {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
-    if(x < size)
+    if (x < size)
     {
         T max_bin = histogram[0];
         T min_bin = histogram[0];
         // first find the biggest bin
-        for(int i = 1; i < size; ++i)
+        for (int i = 1; i < size; ++i)
         {
             max_bin = max(max_bin, histogram[i]);
             min_bin = min(min_bin, histogram[i]);
@@ -94,18 +91,17 @@ __global__ void draw_kernel(cv::cudev::PtrStepSz<cv::Vec3b> draw, const T* histo
         float scaled = float(value - min_bin) / float(max_bin - min_bin);
         int start = draw.rows * (1 - scaled);
         start = min(draw.rows - 1, max(0, start));
-        while(start < draw.rows)
+        while (start < draw.rows)
         {
-    #pragma unroll
-            for(int i = 0; i < 3; ++i)
+#pragma unroll
+            for (int i = 0; i < 3; ++i)
                 draw(start, x).val[i] = 255;
             ++start;
         }
     }
-
 }
 
-template<int N>
+template <int N>
 void launch_uchar(cv::cuda::GpuMat& draw, const cv::cuda::GpuMat& hist, cv::cuda::Stream& stream)
 {
     CV_Assert(draw.depth() == CV_8U);
@@ -114,11 +110,11 @@ void launch_uchar(cv::cuda::GpuMat& draw, const cv::cuda::GpuMat& hist, cv::cuda
     CV_Assert(hist.depth() == CV_32S);
     CV_Assert(hist.rows == 1);
 
-    draw_uchar_kernel<N><<<1, 256, 0, cv::cuda::StreamAccessor::getStream(stream)>>>(draw, (const int*)hist.data, hist.cols);
+    draw_uchar_kernel<N>
+        <<<1, 256, 0, cv::cuda::StreamAccessor::getStream(stream)>>>(draw, (const int*)hist.data, hist.cols);
 }
 
-
-template<class T, int N>
+template <class T, int N>
 void launch(cv::cuda::GpuMat& draw, const cv::cuda::GpuMat& hist, cv::cuda::Stream& stream)
 {
     CV_Assert(draw.depth() == CV_8U);
@@ -127,12 +123,10 @@ void launch(cv::cuda::GpuMat& draw, const cv::cuda::GpuMat& hist, cv::cuda::Stre
     CV_Assert(hist.depth() == cv::DataType<T>::depth);
     CV_Assert(hist.rows == 1);
     cudaStream_t _stream = cv::cuda::StreamAccessor::getStream(stream);
-    draw_kernel<T, N><<<1, 256, 0, _stream>>>(draw,
-                                              (const T*)hist.data,
-                                              hist.cols);
+    draw_kernel<T, N><<<1, 256, 0, _stream>>>(draw, (const T*)hist.data, hist.cols);
 }
 
-template<class T>
+template <class T>
 void launch(cv::cuda::GpuMat& draw, const cv::cuda::GpuMat& hist, cv::cuda::Stream& stream)
 {
     CV_Assert(draw.depth() == CV_8U);
@@ -141,39 +135,32 @@ void launch(cv::cuda::GpuMat& draw, const cv::cuda::GpuMat& hist, cv::cuda::Stre
     CV_Assert(hist.depth() == cv::DataType<T>::depth);
     CV_Assert(hist.rows == 1);
     cudaStream_t _stream = cv::cuda::StreamAccessor::getStream(stream);
-    draw_kernel<T><<<1, 256, 0, _stream>>>(draw,
-                                              (const T*)hist.data,
-                                              hist.cols);
+    draw_kernel<T><<<1, 256, 0, _stream>>>(draw, (const T*)hist.data, hist.cols);
 }
 void cv::cuda::drawHistogram(cv::InputArray histogram,
-                   cv::OutputArray draw_,
-                   cv::InputArray bins,
-                   cv::cuda::Stream& stream)
+                             cv::OutputArray draw_,
+                             cv::InputArray bins,
+                             cv::cuda::Stream& stream)
 {
     drawPlot(histogram, draw_, stream);
 }
 
-void cv::cuda::drawPlot(cv::InputArray arr,
-                   cv::OutputArray draw_,
-                   cv::cuda::Stream& stream)
+void cv::cuda::drawPlot(cv::InputArray arr, cv::OutputArray draw_, cv::cuda::Stream& stream)
 {
     CV_Assert(arr.depth() == CV_32S || arr.depth() == CV_32F);
     cv::cuda::GpuMat& draw = draw_.getGpuMatRef();
-    if(draw.rows != 100 || draw.cols != arr.cols())
+    if (draw.rows != 100 || draw.cols != arr.cols())
     {
         draw.create(100, arr.cols(), CV_MAKE_TYPE(CV_8U, arr.channels()));
     }
 
     draw.setTo(cv::Scalar::all(0), stream);
-    typedef void(*func_t)(cv::cuda::GpuMat& draw, const cv::cuda::GpuMat& hist, cv::cuda::Stream& stream);
-    func_t funcs[4][7] =
-    {
-        // CV_8U, CV_8S, CV_16U, CV_16S, CV_32S, CV_32F, CV_64F
-        {launch<uchar>, 0, 0, 0, launch<int>, launch<float>, 0},
-        {launch<uchar,2>, 0, 0, 0, launch<int,2>, launch<float, 2>, 0},
-        {launch<uchar,3>, 0, 0, 0, launch<int,3>, launch<float, 3>, 0},
-        {launch<uchar,4>, 0, 0, 0, launch<int,4>, launch<float, 4>, 0}
-    };
-    CV_Assert(funcs[arr.channels() -1][arr.depth()]);
+    typedef void (*func_t)(cv::cuda::GpuMat & draw, const cv::cuda::GpuMat& hist, cv::cuda::Stream& stream);
+    func_t funcs[4][7] = {// CV_8U, CV_8S, CV_16U, CV_16S, CV_32S, CV_32F, CV_64F
+                          {launch<uchar>, 0, 0, 0, launch<int>, launch<float>, 0},
+                          {launch<uchar, 2>, 0, 0, 0, launch<int, 2>, launch<float, 2>, 0},
+                          {launch<uchar, 3>, 0, 0, 0, launch<int, 3>, launch<float, 3>, 0},
+                          {launch<uchar, 4>, 0, 0, 0, launch<int, 4>, launch<float, 4>, 0}};
+    CV_Assert(funcs[arr.channels() - 1][arr.depth()]);
     funcs[arr.channels() - 1][arr.depth()](draw, arr.getGpuMat(), stream);
 }
--- plugins/aqcore/src/aqcore/display/LabelDisplay.cu
+++ plugins/aqcore/src/aqcore/display/LabelDisplay.cu
@@ -1,35 +1,37 @@
 
 #include <Aquila/utilities/thrust/thrust_interop.hpp>
-#include <vector_types.h>
+#include <opencv2/core/cuda/type_traits.hpp>
+#include <opencv2/core/cuda/utility.hpp>
+#include <opencv2/core/cuda/vec_traits.hpp>
+#include <opencv2/core/cuda_stream_accessor.hpp>
+#include <thrust/execution_policy.h>
 #include <thrust/for_each.h>
 #include <thrust/iterator/zip_iterator.h>
-#include <thrust/execution_policy.h>
 #include <thrust/transform.h>
-#include <opencv2/core/cuda_stream_accessor.hpp>
-#include <opencv2/core/cuda/utility.hpp>
-#include <opencv2/core/cuda/type_traits.hpp>
-#include <opencv2/core/cuda/vec_traits.hpp>
+#include <vector_types.h>
 
-
-template<class T>
-void __global__ apply_lut_kernel(const cv::Vec3b* lut, const cv::cuda::PtrStepSz<T> in, cv::cuda::PtrStepSz<cv::Vec3b> out, int num_labels)
+template <class T>
+void __global__ apply_lut_kernel(const cv::Vec3b* lut,
+                                 const cv::cuda::PtrStepSz<T> in,
+                                 cv::cuda::PtrStepSz<cv::Vec3b> out,
+                                 int num_labels)
 {
     const int x = blockDim.x * blockIdx.x + threadIdx.x;
     const int y = blockDim.y * blockIdx.y + threadIdx.y;
-    if(x < in.cols && y < in.rows)
+    if (x < in.cols && y < in.rows)
     {
-        T idx = in(y,x);
-        if(idx >= 0 && idx < num_labels)
-            out(y,x) = lut[idx];
+        T idx = in(y, x);
+        if (idx < num_labels)
+            out(y, x) = lut[idx];
     }
 }
 
 namespace aq
 {
     void applyColormap(const cv::cuda::GpuMat& input_8U,
-                                 cv::cuda::GpuMat& output_8UC3,
-                                 const cv::cuda::GpuMat& colormap,
-                                 cv::cuda::Stream& stream)
+                       cv::cuda::GpuMat& output_8UC3,
+                       const cv::cuda::GpuMat& colormap,
+                       cv::cuda::Stream& stream)
     {
         CV_Assert(input_8U.size().area());
         CV_Assert(input_8U.depth() == CV_8U || input_8U.depth() == CV_32S);
@@ -39,15 +41,12 @@ namespace aq
 
         output_8UC3.create(input_8U.rows, input_8U.cols, CV_8UC3);
         dim3 block(32, 8);
-        dim3 grid(cv::cuda::device::divUp(input_8U.cols, block.x),
-                  cv::cuda::device::divUp(input_8U.rows, block.y));
-        if(input_8U.depth() == CV_8U)
-            apply_lut_kernel<uchar><<<grid, block, 0, cv::cuda::StreamAccessor::getStream(stream)>>>(colormap.ptr<cv::Vec3b>(),
-                                                                                          input_8U, output_8UC3, colormap.cols);
+        dim3 grid(cv::cuda::device::divUp(input_8U.cols, block.x), cv::cuda::device::divUp(input_8U.rows, block.y));
+        if (input_8U.depth() == CV_8U)
+            apply_lut_kernel<uchar><<<grid, block, 0, cv::cuda::StreamAccessor::getStream(stream)>>>(
+                colormap.ptr<cv::Vec3b>(), input_8U, output_8UC3, colormap.cols);
         else
-            apply_lut_kernel<int><<<grid, block, 0, cv::cuda::StreamAccessor::getStream(stream)>>>(colormap.ptr<cv::Vec3b>(),
-                                                                                          input_8U, output_8UC3, colormap.cols);
+            apply_lut_kernel<int><<<grid, block, 0, cv::cuda::StreamAccessor::getStream(stream)>>>(
+                colormap.ptr<cv::Vec3b>(), input_8U, output_8UC3, colormap.cols);
     }
-}
-
-
+} // namespace aq
