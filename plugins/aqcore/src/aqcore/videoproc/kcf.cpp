8a51492f8b06dfa5c0c15a7d093fad00569e73d5
dan <dtmoodie@gmail.com>
2022-04-03 15:56:00 -0400

    wip kcf tracker

---
+++ plugins/aqcore/src/aqcore/videoproc/kcf.cpp
@@ -0,0 +1,91 @@
+#include "kcf.hpp"
+#include <Aquila/nodes/NodeInfo.hpp>
+#include <Aquila/types/ObjectTracking.hpp>
+
+namespace aqcore
+{
+    bool TrackerKCF::processImpl() { return false; }
+
+    bool TrackerKCF::processImpl(mo::IAsyncStream& stream)
+    {
+        cv::Mat img = image->getMat(&stream);
+        auto boxes = detections->getComponent<aq::detection::BoundingBox2d>();
+        auto IDs = detections->getComponent<aq::detection::Id>();
+        const size_t num_dets = boxes.getShape()[0];
+        if (!m_tracker)
+        {
+            cv::TrackerKCF::Params params;
+            params.detect_thresh = detect_thresh;
+            params.sigma = sigma;
+            params.lambda = lambda;
+            params.interp_factor = interp_factor;
+            params.output_sigma_factor = output_sigma_factor;
+            params.pca_learning_rate = pca_learning_rate;
+            params.resize = resize;
+            params.split_coeff = split_coeff;
+            params.wrap_kernel = wrap_kernel;
+            params.compress_feature = compress_feature;
+            params.max_patch_size = max_patch_size;
+            params.compressed_size = compressed_size;
+            params.desc_pca = desc_pca;
+            params.desc_npca = desc_npca;
+            m_tracker = cv::TrackerKCF::create(params);
+            m_tracker->init(img, boxes[0]);
+            m_previous_id = IDs[0];
+            return true;
+        }
+        cv::Rect bb;
+        const bool success = m_tracker->update(img, bb);
+
+        auto output = *detections;
+
+        if (success)
+        {
+            // now we iterate over all current detections and associate to a current detection
+            float best_iou = 0.0F;
+            int32_t best_iou_match = -1;
+            for (size_t i = 0; i < num_dets; ++i)
+            {
+                const float iou = aq::iou(bb, boxes[i]);
+                if (iou > iou_threshold)
+                {
+                    if (iou > best_iou)
+                    {
+                        best_iou = iou;
+                        best_iou_match = i;
+                    }
+                }
+            }
+            if (best_iou_match != -1)
+            {
+                // update the database with the new ID,
+                auto current_id = IDs[best_iou_match];
+                // check the current ID
+                if (current_id != m_previous_id)
+                {
+                    // Need to update the current ID and all other IDs in this detection set
+                    auto IDs = output.getComponentMutable<aq::detection::Id>();
+                    IDs[best_iou_match] = m_previous_id;
+
+                    // Now make the ID unique
+                }
+            }
+            else
+            {
+                // We have not found a match, so add this detection to the database
+            }
+        }
+
+        this->output.publish(output, mo::tags::param = &image_param);
+
+        return true;
+    }
+
+    bool TrackerKCF::processImpl(mo::IDeviceStream& stream)
+    {
+        return processImpl(static_cast<mo::IAsyncStream&>(stream));
+    }
+} // namespace aqcore
+
+using namespace aqcore;
+MO_REGISTER_CLASS(TrackerKCF)
---
+++ plugins/aqcore/src/aqcore/videoproc/kcf.hpp
@@ -0,0 +1,59 @@
+#ifndef AQCORE_KCF_HPP
+#define AQCORE_KCF_HPP
+#include <ct/types/opencv.hpp>
+
+#include <Aquila/nodes/Node.hpp>
+
+#include <Aquila/types/ObjectDetection.hpp>
+#include <Aquila/types/SyncedImage.hpp>
+
+#include <MetaObject/params/ParamMacros.hpp>
+
+#include <opencv2/tracking/tracking.hpp>
+
+namespace aqcore
+{
+    class TrackerKCF : public aq::nodes::Node
+    {
+      public:
+        using Components_t = ct::VariadicTypedef<aq::detection::BoundingBox2d, aq::detection::Id>;
+        using Input_t = aq::TDetectedObjectSet<Components_t>;
+        using Output_t = Input_t;
+
+        MO_DERIVE(TrackerKCF, aq::nodes::Node)
+            INPUT(aq::SyncedImage, image)
+            INPUT(Input_t, detections)
+
+            OUTPUT(Output_t, output)
+
+            PARAM(float, detect_thresh, 0.5F)               //!<  detection confidence threshold
+            PARAM(float, sigma, 0.2F)                       //!<  gaussian kernel bandwidth
+            PARAM(float, lambda, 0.0001F)                   //!<  regularization
+            PARAM(float, interp_factor, 0.075F)             //!<  linear interpolation factor for adaptation
+            PARAM(float, output_sigma_factor, 1.0F / 16.0F) //!<  spatial bandwidth (proportional to target)
+            PARAM(float, pca_learning_rate, 0.15F)          //!<  compression learning rate
+            PARAM(bool, resize, true)           //!<  activate the resize feature to improve the processing speed
+            PARAM(bool, split_coeff, true)      //!<  split the training coefficients into two matrices
+            PARAM(bool, wrap_kernel, false)     //!<  wrap around the kernel values
+            PARAM(bool, compress_feature, true) //!<  activate the pca method to compress the features
+            PARAM(int, max_patch_size, 80 * 80) //!<  threshold for the ROI size
+            PARAM(int, compressed_size, 2)      //!<  feature size after compression
+            PARAM(int, desc_pca, cv::TrackerKCF::MODE::CN)    //!<  compressed descriptors of TrackerKCF::MODE
+            PARAM(int, desc_npca, cv::TrackerKCF::MODE::GRAY) //!<  non-compressed descriptors of TrackerKCF::MODE
+
+            PARAM(float, iou_threshold, 0.6F)
+        MO_END;
+
+        bool processImpl() override;
+        bool processImpl(mo::IAsyncStream& stream) override;
+        bool processImpl(mo::IDeviceStream& stream) override;
+
+      private:
+        cv::Ptr<cv::TrackerKCF> m_tracker;
+        boost::optional<mo::Header> m_previous_header;
+        // Should save more than just the previous ID, we should save all components
+        aq::detection::Id m_previous_id;
+    };
+} // namespace aqcore
+
+#endif // AQCORE_KCF_HPP
